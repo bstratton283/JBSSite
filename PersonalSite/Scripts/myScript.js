809216450fa6f930f814daa356d9fbce5da0c0b2
brad.stratton <bstratton283@gmail.com>
2017-06-05 10:34:21 -0400

    Styled error pages and started footer content

--- PersonalSite/Scripts/myScript.js
+++ PersonalSite/Scripts/myScript.js
@@ -1,6 +1,15 @@
-﻿//Open navbar's 2nd level dropdown
-$('.dropdown-submenu a.dropdown-submenu-triggerbutton').click(function (e) {
-    $(this).next('ul').toggle();
-    e.stopPropagation();
-    e.preventDefault();
-});
+﻿$(function () {
+
+    //Open navbar's 2nd level dropdown
+    $('.dropdown-submenu a.dropdown-submenu-triggerbutton').click(function (e) {
+        $(this).next('ul').toggle();
+        e.stopPropagation();
+        e.preventDefault();
+    });
+
+    //Calculate the footer stats
+    $("#line-count").html(
+        $(".body-content > .prettyprint > ol.linenums > li").length
+    );
+
+})
